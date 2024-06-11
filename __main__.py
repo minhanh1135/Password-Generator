aa622d65f90c1d483fdbf35576baad2987a3730c
minhanh1135 <64486608+minhanh1135@users.noreply.github.com>
2024-06-11 22:05:23 +0700

    Implement password generation with user choice
    - Added the main `password()` function that handles the password generation process
    - The function first checks if the user's input password is valid
    - If not, it prompts the user to choose between a random password or modifying the existing one
    - If the user chooses random, the function generates a new password that meets the requirements
    - If the user chooses to modify, the function adds random characters to the end of the existing password until it's valid
    - The function returns the final password

---
+++ __main__.py
@@ -0,0 +1,49 @@
+import random
+from password_validator import valid_password
+from password_generator import replaceWithNumber, replaceWithSpecialNumber, replaceWithUpper
+
+def password(pword_user):
+    pwd, wcontinue = valid_password(pwd=pword_user, show_warning=True)
+
+    if wcontinue == False:
+        print('The password is valid!')
+        return pwd
+
+    elif wcontinue == True:
+        print("The password doesn't meet the requirements.")
+        user_choice = input("Do you want to use a random password (r) or modify the existing password (m)? ")
+
+        if user_choice.lower() == 'r':
+            password2 = ""
+            pwd2, wcontinue2 = valid_password(pwd=password2)
+
+            while wcontinue2 == True:
+                pwlength = random.randrange(10, 21)
+                alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+                for j in range(pwlength):
+                    next_letter_index = random.randrange(len(alphabet))
+                    password2 = password2 + alphabet[next_letter_index]
+
+                password2 = replaceWithNumber(password2, pwlength)
+                password2 = replaceWithSpecialNumber(password2, pwlength)
+                password2 = replaceWithUpper(password2, pwlength)
+
+                password2, wcontinue2 = valid_password(pwd=password2)
+
+            print('New password:', password2)
+            return password2
+
+        elif user_choice.lower() == 'm':
+            pwd2 = pword_user
+            while wcontinue == True:
+                pwlength = len(pwd2)
+                alphabet = "abcdefghijklmnopqrstuvwxyz"
+                pwd2 = pwd2 + alphabet[random.randrange(len(alphabet))]
+                pwd2, wcontinue = valid_password(pwd=pwd2)
+            print('Modified password:', pwd2)
+            return pwd2
+
+        else:
+            print("Invalid choice. Please try again.")
+            return None
